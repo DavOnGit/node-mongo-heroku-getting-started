5e161afb45048c8be5cd6b72a1ef20b54c1c28c4
Davide Vozzi <davide.vozzi@gmail.com>
2016-09-21 21:57:12 +0200

    hide DB credentials

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const cool = require('cool-ascii-faces');
 const bodyParser= require('body-parser');
+var conf = require('./config');
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+
 var db;
+var urlDB = (process.env.MONGODB_URI || conf.MONGODB_URI);	// This is for Heroku
 
 app.set('port', (process.env.PORT || 5000));
 
-MongoClient.connect('mongodb://heroku_r1hdctpj:<PASSWORD>@ds029436.mlab.com:29436/heroku_r1hdctpj', (err, database) => {
+MongoClient.connect(urlDB, (err, database) => {
   if (err) return console.log(err);
   db = database;
   app.listen(app.get('port'), () => {
-    console.log('listening on ' + app.get('port'))
+    console.log('listening on port ' + app.get('port'))
   });
 });
 
@@ -32,11 +35,10 @@ app.get('/cool', function(request, response) {
 });
 
 app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+    var result = '';
+    var times = process.env.TIMES || 5;
+    for (i=0; i < times; i++) result += i + ' ';
+    response.send(result);
 });
 
 app.get('/db', function(request, response) {
@@ -70,10 +72,9 @@ app.put('/quotes', (req, res) => {
       upsert: true
     },
     (err, result) => {
-      if (err) return res.send(err);
+      if (err) return res.send(500, err);
       res.send(result);
     }
-  //res.redirect('/db');
   );
 });
 
